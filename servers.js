a5502a4332f59108bb887db23f297111787cdc19
pentreathm <pentreathm@decentraland.org>
2026-04-22 14:56:19 -0300

    Update servers.js
    Signed-off-by: pentreathm <pentreathm@decentraland.org>

--- servers.js
+++ servers.js
@@ -1,11 +1,9 @@
 const DAOMainnetServers = [
   "https://peer-ec1.decentraland.org",
   "https://peer-ec2.decentraland.org",
-  "https://peer-wc1.decentraland.org",
   "https://peer-eu1.decentraland.org",
   "https://peer-ap1.decentraland.org",
   "https://interconnected.online",
-  "https://peer.decentral.io",
   "https://peer.melonwave.com",
   "https://peer.kyllian.me",
   "https://peer.uadevops.com",
